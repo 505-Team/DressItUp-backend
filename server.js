bbba188db98787b608e05a394e76bac5a22bf93f
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-09-19 13:28:41 +0300

    merged backend stuff

--- server.js
+++ server.js
@@ -90,54 +90,51 @@ async function main() {
   // seedData();
 }
 
-async function seedData() {
-  const CentralPark = new PaintingModel({
-    title: 'Central Park',
-    imgUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1442582252l/26648244._SX318_.jpg',
-    description: 'Alice and Gabriel have no memory of the night before…yet they won’t forget it any­time soon.New York, 8am. Alice, a young Parisian cop and Gabriel, American jazz pianist, wake up on a bench in Central park hand­cuffed to one another. They don’t know each other and have no memory of having met. The night before, Alice was at a party with her girl­friends on the Champs-Elysées and Gabriel was playing piano in a club in Dublin.Impossible? And yet… So many ques­tions leave them con­founded. How did they get them­selves into such a dan­gerous sit­u­a­tion? Whose blood has stained Alice’s shirt? Why is one bullet missing from her gun?Alice and Gabriel are left with no choice but to team up to figure out what is hap­pening to them and get back to their normal lives. What they are going to dis­cover will turn their lives upside down.',
-    status: 'finished,free',
-    painter: 'meso',
-    ownerEmail: 'malakkhasawneh2@gmail.com'
-  });
+// async function seedData() {
+//   const CentralPark = new PaintingModel({
+//     title: 'Central Park',
+//     imgUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1442582252l/26648244._SX318_.jpg',
+//     description: 'Alice and Gabriel have no memory of the night before…yet they won’t forget it any­time soon.New York, 8am. Alice, a young Parisian cop and Gabriel, American jazz pianist, wake up on a bench in Central park hand­cuffed to one another. They don’t know each other and have no memory of having met. The night before, Alice was at a party with her girl­friends on the Champs-Elysées and Gabriel was playing piano in a club in Dublin.Impossible? And yet… So many ques­tions leave them con­founded. How did they get them­selves into such a dan­gerous sit­u­a­tion? Whose blood has stained Alice’s shirt? Why is one bullet missing from her gun?Alice and Gabriel are left with no choice but to team up to figure out what is hap­pening to them and get back to their normal lives. What they are going to dis­cover will turn their lives upside down.',
+//     status: 'finished,free',
+//     painter: 'meso',
+//     ownerEmail: 'malakkhasawneh2@gmail.com'
+//   });
 
-  const MonteCristo = new PaintingModel({
-    title: 'The Count of Monte Cristo',
-    imgUrl: 'https://payload.cargocollective.com/1/6/222472/6354649/jhuusko_montecristo_Front_o.jpg',
-    description: 'Thrown in prison for a crime he has not committed, Edmond Dantes is confined to the grim fortress of If. There he learns of a great hoard of treasure hidden on the Isle of Monte Cristo and he becomes determined not only to escape, but also to unearth the treasure and use it to plot the destruction of the three men responsible for his incarceration. Dumas’ epic tale of suffering and retribution, inspired by a real-life case of wrongful imprisonment, was a huge popular success when it was first serialized in the 1840s.',
-    status: 'finished,free',
-    painter: 'meso',
+//   const MonteCristo = new PaintingModel({
+//     title: 'The Count of Monte Cristo',
+//     imgUrl: 'https://payload.cargocollective.com/1/6/222472/6354649/jhuusko_montecristo_Front_o.jpg',
+//     description: 'Thrown in prison for a crime he has not committed, Edmond Dantes is confined to the grim fortress of If. There he learns of a great hoard of treasure hidden on the Isle of Monte Cristo and he becomes determined not only to escape, but also to unearth the treasure and use it to plot the destruction of the three men responsible for his incarceration. Dumas’ epic tale of suffering and retribution, inspired by a real-life case of wrongful imprisonment, was a huge popular success when it was first serialized in the 1840s.',
+//     status: 'finished,free',
+//     painter: 'meso',
 
-    ownerEmail: 'malakkhasawneh2@gmail.com'
-  });
-  const SherlockHolmes = new PaintingModel({
-    title: 'A Study in Scarlet',
-    imgUrl: 'https://m.media-amazon.com/images/I/51JdmxJ8b7L.jpg',
-    description: 'A Study in Scarlet" is the first published story of one of the most famous literary detectives of all time, Sherlock Holmes. Here Dr. Watson, who has just returned from a war in Afghanistan, meets Sherlock Holmes for the first time when they become flat-mates at the famous 221 B Baker Street. In "A Study in Scarlet" Sherlock Holmes investigates a murder at Lauriston Gardens as Dr. Watson tags along with Holmes while narratively detailing his amazing deductive abilities.',
-    status: 'finished,free',
-    painter: 'meso',
+//     ownerEmail: 'malakkhasawneh2@gmail.com'
+//   });
+//   const SherlockHolmes = new PaintingModel({
+//     title: 'A Study in Scarlet',
+//     imgUrl: 'https://m.media-amazon.com/images/I/51JdmxJ8b7L.jpg',
+//     description: 'A Study in Scarlet" is the first published story of one of the most famous literary detectives of all time, Sherlock Holmes. Here Dr. Watson, who has just returned from a war in Afghanistan, meets Sherlock Holmes for the first time when they become flat-mates at the famous 221 B Baker Street. In "A Study in Scarlet" Sherlock Holmes investigates a murder at Lauriston Gardens as Dr. Watson tags along with Holmes while narratively detailing his amazing deductive abilities.',
+//     status: 'finished,free',
+//     painter: 'meso',
 
-    ownerEmail: 'malakkhasawneh2@gmail.com'
-  });
+//     ownerEmail: 'malakkhasawneh2@gmail.com'
+//   });
 
 
-  await CentralPark.save();
-  await MonteCristo.save();
-  await SherlockHolmes.save();
-}
+//   await CentralPark.save();
+//   await MonteCristo.save();
+//   await SherlockHolmes.save();
+// }
 
 
 //Routes
-server.get('/', homeHandler);
-server.get('/getPainting', getPaintingHandler);
+
+// server.get('/getPainting', getPaintingHandler);
 server.post('/addPainting', addPaintingHandler);
 server.delete('/deletePainting/:id', deletePaintingHandler);
 server.put('/updatePainting/:id',updatePaintingHandler);
 
 //Functions Handlers
-function homeHandler(req, res) {
 
-  res.send('Home Page');
-}
 
 function getPaintingHandler(req, res) {
   //send fav books list (email)
